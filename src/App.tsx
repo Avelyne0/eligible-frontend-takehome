b6550aabf33273e6f81d2c52bac265f71925f061
Sam Barrie <sam.dbarrie@gmail.com>
2021-06-10 12:45:18 +0100

    other crap

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
-import React from "react"
-import Posts from "./Posts"
-import { Redirect, Route, Switch } from "react-router"
-import PostDetail from "./PostDetail"
+import React from "react";
+import { Redirect, Route, Switch } from "react-router";
+import PostDetail from "./components/PostDetail";
+import Posts from "./components/Posts";
 
 export default () => (
   <div>
@@ -11,4 +11,4 @@ export default () => (
       <Redirect to="/" />
     </Switch>
   </div>
-)
+);
